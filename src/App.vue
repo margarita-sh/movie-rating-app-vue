3da7349861cced555c697f73b26893ae1c270f9c
Marharyta Shaduryna <margaritka1992@gmail.com>
2025-06-11 23:34:15 +0300

    feat: implement movie search

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { items } from "./movies.json";
+import Card from "./Card.vue";
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-900 p-8">
+    <div class="flex justify-center content-center">
+      <Card v-for="movie in items" :movieProps="movie" />
+    </div>
+  </div>
+</template>
---
+++ src/Card.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { StarIcon } from "@heroicons/vue/24/solid";
+
+defineProps({
+    movieProps: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div :id="movieProps.id" class="bg-white rounded-lg shadow-md overflow-hidden w-72 m-4">
+        <img :src="movieProps.image" alt="movie poster" class="w-full h-96 object-cover"/>
+        <div class="p-4">
+      <h3 class="text-lg font-semibold text-gray-900 mb-2">{{ movieProps.name }}</h3>
+      <div class="flex flex-wrap gap-2 mb-2">
+        <span
+          v-for="(genre, index) in movieProps.genres"
+          :key="index"
+          class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-full"
+        >
+          {{ genre }}
+        </span>
+      </div>
+      <p class="text-sm text-gray-700 mb-3">{{ movieProps.description }}</p>
+      <div class="flex items-center">
+        <span class="text-sm text-gray-600 mr-2">Rating: ({{ movieProps.rating }}/5)</span>
+          <StarIcon
+            v-for="i in 5"
+            :key="i"
+            class="w-4 h-4"
+            :class="i <= movieProps.rating ? 'text-yellow-400' : 'text-gray-300'"
+          />
+      </div>
+    </div>
+    </div>
+</template>
